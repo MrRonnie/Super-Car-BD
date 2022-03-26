7f5b55c72f1d9a5532ee0fd0ba821bb2e0ae5700
MrRonnie <ronymaza55@gmail.com>
2022-03-26 23:21:53 +0600

    cart add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = () => {
+
+const Cart = ({name , img}) => {
+    
     return (
-        <div>
-            
-        </div>
+        <div className='selected-carName-img'>
+            <img className='small-rounded-img' src={img} alt="" />
+            <p className='selected-car'> {name}</p>
+       </div>
     );
 };
 
